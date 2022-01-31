a028fe846b906fdb6f94d612c9e88bf82299e59e
Sami Yaseen <sami.yaseen@tilotech.io>
2022-01-31 14:52:17 +0100

    Fix deployment

--- internal/pkg/step/terraformapply.go
+++ internal/pkg/step/terraformapply.go
@@ -1,8 +1,11 @@
 package step
 
+import "os"
+
 func TerraformApply(workspace string, args ...string) Step {
 	args = append([]string{"apply", "-auto-approve"}, args...)
 	command := createCommand("terraform", args...)
+	command.Env = os.Environ()
 	command.Env = append(command.Env, "TF_WORKSPACE="+workspace)
 	return runCommand("could not apply: %v", command)
 }
--- internal/pkg/step/terraforminit.go
+++ internal/pkg/step/terraforminit.go
@@ -1,7 +1,10 @@
 package step
 
+import "os"
+
 func TerraformInit() error {
 	command := createCommand("terraform", "init")
+	command.Env = os.Environ()
 	command.Env = append(command.Env, "TF_WORKSPACE=default")
 	return runCommand("could not initialize terraform: %v", command)()
 }
