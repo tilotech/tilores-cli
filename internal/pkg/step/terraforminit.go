31d36e81f80f2b92fb1d77b4a5e64685f26b81ba
Sami Yaseen <sami.yaseen@tilotech.io>
2022-02-02 15:00:36 +0100

    Upgrade terraform dependencies on init

--- internal/pkg/step/terraforminit.go
+++ internal/pkg/step/terraforminit.go
@@ -1,7 +1,7 @@
 package step
 
 func TerraformInit() error {
-	command := createCommand("terraform", "init")
+	command := createCommand("terraform", "init", "-upgrade")
 	command.Env = append(command.Env, "TF_WORKSPACE=default")
 	return runCommand("could not initialize terraform: %v", command)()
 }
