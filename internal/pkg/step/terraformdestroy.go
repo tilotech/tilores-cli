dee78e1547abc1d036b5b5c834f9c2cfd26067df
Stefan Berkner <stefan.berkner@tilotech.io>
2024-01-25 16:49:48 +0100

    Fix linter

--- internal/pkg/step/terraformdestroy.go
+++ internal/pkg/step/terraformdestroy.go
@@ -4,6 +4,6 @@ package step
 func TerraformDestroy(workspace string, args ...string) Step {
 	args = append([]string{"destroy", "-auto-approve"}, args...)
 	command := createCommand("terraform", args...)
-	command.Env = append(command.Env, "TF_WORKSPACE="+workspace)
+	command.Env = append(command.Env, tfWorkspaceEnv(workspace))
 	return runCommand("could not destroy: %v", command)
 }
